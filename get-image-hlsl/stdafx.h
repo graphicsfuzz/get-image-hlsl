64a643a2b0bf201058bb46b8493ea5ba84f8aef4
David R. MacIver <david@drmaciver.com>
2017-05-09 11:35:56 +0100

    Initial checkin of code

--- get-image-hlsl/stdafx.h
+++ get-image-hlsl/stdafx.h
@@ -5,11 +5,50 @@
 
 #pragma once
 
+#define _WIN32_WINNT 0x600
+
 #include "targetver.h"
 
 #include <stdio.h>
+#include <iostream>
+
 #include <tchar.h>
 
+#include <d3dcompiler.h>
 
+#include <d3d11_3.h>
+#include <d2d1_3.h>
+#include <d2d1effects_2.h>
+#include <dwrite_3.h>
+#include <wincodec.h>
+#include <DirectXColors.h>
+#include <DirectXMath.h>
+#include <memory>
+#include <concrt.h>
+#include <Windows.h>
+#include <wrl/client.h>
 
-// TODO: reference additional headers your program requires here
+// " to simplify the tutorial we will go ahead and add them all to your new project's pch.h header" lol
+
+#include "CommonStates.h"
+#include "DDSTextureLoader.h"
+#include "DirectXHelpers.h"
+#include "Effects.h"
+#include "GamePad.h"
+#include "GeometricPrimitive.h"
+#include "GraphicsMemory.h"
+#include "Keyboard.h"
+#include "Model.h"
+#include "Mouse.h"
+#include "PrimitiveBatch.h"
+#include "ScreenGrab.h"
+#include "SimpleMath.h"
+#include "SpriteBatch.h"
+#include "SpriteFont.h"
+#include "VertexTypes.h"
+#include "WICTextureLoader.h"
+
+#include <comdef.h>
+
+#pragma comment(lib,"d3dcompiler.lib")
+#pragma comment(lib, "d3d11.lib")
